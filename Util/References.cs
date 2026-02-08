cc5e7981e20bf0989e1105b5524eaffee585b819
Zorian1337 <SympathyGaming1337@gmail.com>
2026-02-08 14:08:19 -0500

    adding a certain stage for where we can always have access to important information in the game

---
+++ Util/References.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TheForest.Utils;
+
+namespace Forest_Mod.Util
+{
+    public static class References
+    {
+        public static LocalPlayer Player { get; private set; }
+        public static void SetLocalPlayer(ref LocalPlayer _Player) => Player = _Player;
+    }
+}
