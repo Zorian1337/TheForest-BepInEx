a47c52fb0332177f4d97300f509635ecaac9feb2
Zorian1337 <SympathyGaming1337@gmail.com>
2026-02-07 23:49:59 -0500

    added missing assembly info and properties

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿// C:\Users\Sympathy\Downloads\Forest-Mod.dll
+// Forest-Mod, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+
+// Timestamp: <Unknown> (C819BCD0)
+
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: CompilationRelaxations(8)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Forest-Mod")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e812dfaa-7a51-4440-9dfc-ab0f6f3efcb7")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyTitle("Forest-Mod")]
