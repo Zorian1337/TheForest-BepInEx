075fbb031f458895d9474f66427cef5480a1899a
Zorian1337 <SympathyGaming1337@gmail.com>
2026-02-08 17:56:39 -0500

    Disabled the blur mode on the post effects

---
+++ Patches/WaterBlur_Patch.cs
@@ -0,0 +1,31 @@
+﻿using Ceto;
+using Forest_Mod.Configs;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using TheForest.Graphics;
+using TheForest.Items;
+using TheForest.Items.Inventory;
+using UnityEngine;
+
+namespace Forest_Mod.Patches
+{
+
+    internal class WaterBlur_Patch
+    {
+
+        [HarmonyPatch(typeof(UnderWaterPostEffect), "Start")]
+        class UnderWaterPostEffect_Awake_Patch
+        {
+            static void Postfix(UnderWaterPostEffect __instance)
+            {
+                // Disables underwater blur filter
+                __instance.blurMode = ImageBlur.BLUR_MODE.OFF;
+            }
+        }
+
+    }
+}
