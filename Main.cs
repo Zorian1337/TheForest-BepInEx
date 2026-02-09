4dc1fb2e71199d1abd034e3d0dc85a98d68c76c9
Zorian1337 <SympathyGaming1337@gmail.com>
2026-02-09 16:31:11 -0500

    verify changes (figuring out how github works)

--- Main.cs
+++ Main.cs
@@ -1,35 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using BepInEx;
+﻿using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Logging;
 using Forest_Mod.Configs;
+using Forest_Mod.Custom;
+using Forest_Mod.Util;
 using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using TheForest.Items;
+using UnityEngine;
 
 namespace Forest_Mod
 {
-    // Token: 0x02000002 RID: 2
-    [BepInPlugin("forest.multipatch.zorian", "Forest Example", "1.0")]
+
+
+    [BepInPlugin("forest.multipatch.zorian", ModName, "1.0")]
     public class Main : BaseUnityPlugin
     {
-        // Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+        public const string ModName = TheForest-MultiPatch;
+
         private void Awake()
         {
             Main.Instance = this;
             Main._Logger = base.Logger;
-            base.Logger.LogInfo("ForestExample loaded");
+            base.Logger.LogInfo($"{ModName} loaded");
             Main.ConfigFile = base.Config;
             WireAllConfigs.Wire(base.Config, true);
-            base.Logger.LogInfo("ForestExample checking config");
+            base.Logger.LogInfo("{ModName}} checking config");
             foreach (KeyValuePair<ConfigDefinition, ConfigEntryBase> keyValuePair in base.Config)
             {
                 base.Logger.LogInfo(string.Format("Key: {0}, Value: {1}", keyValuePair.Key, keyValuePair.Value.BoxedValue));
             }
-            Main.harmony = new Harmony("forest.multipatch.zor");
+
+
+            GameObject go = new GameObject("ForestMod_OffhandManager");
+            DontDestroyOnLoad(go);
+            go.AddComponent<OffhandManager>();
+
+            Logger.LogInfo("OffhandManager injected");
+
+            Main.harmony = new Harmony("forest.multipatch.zorian");
             Main.harmony.PatchAll();
         }
 
-        // Token: 0x06000002 RID: 2 RVA: 0x0000212C File Offset: 0x0000032C
+
         private void OnDestroy()
         {
             bool flag = Main.harmony != null;
@@ -41,16 +56,14 @@ namespace Forest_Mod
             WireAllConfigs.Wire(base.Config, true);
         }
 
-        // Token: 0x04000001 RID: 1
+
         public static ConfigFile ConfigFile;
 
-        // Token: 0x04000002 RID: 2
         public static Main Instance;
 
-        // Token: 0x04000003 RID: 3
+
         public static ManualLogSource _Logger;
 
-        // Token: 0x04000004 RID: 4
         public static Harmony harmony;
     }
 }
