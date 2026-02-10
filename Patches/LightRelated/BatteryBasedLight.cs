d5bcae50263e7da55566a49e0fc595b34ad50fd9
Zorian1337 <SympathyGaming1337@gmail.com>
2026-02-10 18:22:57 -0500

    Added direct minute to second calculations for the how long the battery will/can last for future configuration changes

--- Patches/LightRelated/BatteryBasedLight.cs
+++ Patches/LightRelated/BatteryBasedLight.cs
@@ -6,19 +6,30 @@ namespace Forest_Mod.Patches
 {
 
     [HarmonyPatch(typeof(BatteryBasedLight), "Awake")]
-    internal class BatteryBasedLight_Patch
+    public class BatteryBasedLight_Patch
     {
         private static bool Prefix(BatteryBasedLight __instance)
         {
             __instance._mainLight.range = 260f;
             __instance._mainLight.spotAngle = 135f;
             __instance._mainLight.intensity = 30f; //default 22f
-            __instance._batterieCostPerSecond = 0.000833333354f;
+            __instance._batterieCostPerSecond = GetBatteryCostPerSecond(30);//0.000833333354f;
             __instance.SetColor(__instance._torchBaseColor);
 
             netPlayerVis component = __instance.transform.root.GetComponent<netPlayerVis>();
             ReflectionHelper.SetPrivateField(__instance, "_vis", component);
+
+
+
             return false;
         }
+
+        public static float GetBatteryCostPerSecond(int ActiveTimeMinutes)
+        {
+            int ActiveTimeSeconds = ActiveTimeMinutes * 60;
+            return 100f / ActiveTimeSeconds;
+        }
+
+        public static int GetBatteryRemainingTimeInSeconds(float RemainingBattery, float BatteryCostInSeconds) => (RemainingBattery / BatteryCostInSeconds);
     }
 }
