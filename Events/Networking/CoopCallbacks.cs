f0a63adca6b7c666a25aa9a7e0f342667e9eeacf
Zorian1337 <SympathyGaming1337@gmail.com>
2026-02-10 08:05:09 -0500

    added some files for potential networking in the future, nothing of value here just the base files as a backup

---
+++ Events/Networking/CoopCallbacks.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Forest_Mod.Events.Networking
+{
+    internal class CoopCallbacks
+    {
+    }
+}
---
+++ Events/Networking/WireNetEvents.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Forest_Mod.Events.Networking
+{
+    internal class WireNetEvents
+    {
+        public void Reg()
+        {
+            BoltNetwork.RegisterTokenClass
+        }
+        
+    }
+}
