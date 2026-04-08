00c3d92071ce53f91551a833a68b174264501af8
Zorian1337 <SympathyGaming1337@gmail.com>
2026-04-08 19:24:18 -0400

    Used actual method for SetIntensity

--- Patches/LightRelated/BatteryBasedLight.cs
+++ Patches/LightRelated/BatteryBasedLight.cs
@@ -12,7 +12,7 @@ namespace Forest_Mod.Patches
         {
             __instance._mainLight.range = 260f;
             __instance._mainLight.spotAngle = 135f;
-            __instance._mainLight.intensity = 30f; //default 22f
+            __instance.SetIntensity(10f); //was 22f looked bright but not as deadly, 30f was bright asf
             __instance._batterieCostPerSecond = GetBatteryCostPerSecond(30);//0.000833333354f;
             __instance.SetColor(__instance._torchBaseColor);
 
